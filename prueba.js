f73b7ec95d2f890cca211585ddc1f654b0b7d304
christian <cdcb91@hotmail.com>
2019-07-22 21:41:09 -0400

    Initial commit

---
+++ prueba.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    const menu = document.getElementById('menu-options');
+    menu.classList.toggle('is-active');
+  }
+  
+  function redireccionar(){
+    document.location.href="https://wwww.facebook.com";
+  }
+  
